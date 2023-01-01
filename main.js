43b4926f2d06f9d183e8616c63de94c2785e10cf
Jihong Kim <34394165+jihongeek@users.noreply.github.com>
2023-01-01 11:22:19 +0000

    change main.js for KST and add octokit

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
+function toKoreanTime(date) {
+    const koreanTime = date.getTime() + (-date.getTimezoneOffset() + 540) * 1000 * 60
+    return koreanTime
+}
 function getLeftTime(end) {
-    return end - new Date()
+    let nowServerTime = new Date()
+    const nowKoreanTime = nowServerTime.getTime() + (-nowServerTime.getTimezoneOffset() + 540) * 1000 * 60
+    return end - nowKoreanTime
 }
 function getLeftDay(end) {
-    return Math.floor((end - new Date()) / 1000 / 60 / 60 / 24)
+    let nowServerTime = new Date()
+    let nowKoreanTime = nowServerTime.getTime() + (-nowServerTime.getTimezoneOffset() + 540) * 1000 * 60
+    return Math.floor((end - nowKoreanTime) / 1000 / 60 / 60 / 24)
 }
 function dateToString(date) {
     let yearString = date.getFullYear()
@@ -10,17 +18,20 @@ function dateToString(date) {
     let dateString = date.getDate()
     return yearString + '-' + monthString + '-' + dateString
 }
-function getPassedPercent(start,end) {
-    return Math.floor( (new Date() - start) / (end - start) * 100 )
+function getPassedPercent(start, end) {
+    let nowServerTime = new Date()
+    const nowKoreanTime = nowServerTime.getTime() + (-nowServerTime.getTimezoneOffset() + 540) * 1000 * 60
+    return Math.floor((nowKoreanTime - start) / (end - start) * 100)
 }
 function makePercentBar(percent) {
-    let filledCount = Math.floor(percent/10)*2
-    return "█".repeat(filledCount) + "░".repeat(20-filledCount)
+    let filledCount = Math.floor(percent / 10) * 2
+    return "█".repeat(filledCount) + "░".repeat(20 - filledCount)
 }
-let start = new Date(2022,4-1,11,14)
-let end = new Date(2023,10-1,11)
-let passedPercent = getPassedPercent(start,end) 
+let start = new Date(toKoreanTime(new Date(2022, 4 - 1, 11, 14)))
+let end = new Date(toKoreanTime(new Date(2023, 10 - 1, 11)))
+let passedPercent = getPassedPercent(start, end)
 let boxText = `🎉 ROKA 전역(轉役)까지...${getLeftDay(end)}일
 ⏰ ${dateToString(start)} / ${dateToString(end)}
 💌 ${makePercentBar(passedPercent)} ${passedPercent}%`
-console.log(boxText)
+
+
